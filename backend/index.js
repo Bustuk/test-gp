da3afe50770bc31b43c2c5f74c2f2776febbafa6
Łukasz Wołodkiewicz <wolodkiewiczlukasz@gmail.com>
2022-11-18 12:24:27 +0000

    checkpoint-4

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
 const express  = require('express');
 const httpServer = express();
 const dialer = require('dialer').Dialer;
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const config = {
   url: 'https://uni-call.fcc-online.pl',
-  login: '<login>',
-  password: '<haslo>'
+  login: 'focus02',
+  password: ''
 };
 
 dialer.configure(config);  
+
+httpServer.use(bodyParser.json());
+httpServer.use(cors());
+httpServer.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+  next();
+});
+
 
 // Serwer nasłuchuje na porcie 3000
 httpServer.listen(3000, function () {
@@ -17,10 +27,34 @@ httpServer.listen(3000, function () {
   // gp url 3000
 })
 
-httpServer.get('/call/:number1/:number2', (req, res) => {
-  const number1 = req.params.number1;
-  const number2 = req.params.number2;
- 
-  dialer.call(number1,number2).catch(err => { console.log(err) } );
-  res.json({success: true});
-})
+httpServer.post('/call/', async (req, res) => {
+  const number1 = req.body.number;
+  const number2 = 'XXXXXXXXXX' // tutaj dejemy swój numer
+  console.log('Dzwonie', number1, number2)
+  bridge = await dialer.call(number1, number2);
+  let interval = setInterval(async () => {
+    let status = await bridge.getStatus();
+    console.log(status)
+    if (
+      status === "ANSWERED" ||
+      status === "FAILED" ||
+      status === "BUSY" ||
+      status === "NO ANSWER"
+    ) {
+      console.log("stop");
+      clearInterval(interval);
+    }
+  }, 2000);
+  res.json({ success: true });
+ })
+
+
+
+httpServer.get('/test/', async (req, res) => {
+  // const number1 = req.body.number;
+  console.log('WPADA test')
+  const number1 = '792751705'
+  const number2 = '503777158';
+  //await callback(number1, number2)
+  res.json({ success: true });
+ })
