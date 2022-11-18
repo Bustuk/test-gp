58dccf0609ba21b5ed2c7e3fef3ceaeb71012bd9
Łukasz Wołodkiewicz <wolodkiewiczlukasz@gmail.com>
2022-11-18 11:22:08 +0000

    checkpoint-2

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,14 @@ const express  = require('express');
 const httpServer = express();
 const dialer = require('dialer').Dialer;
 
+const config = {
+  url: 'https://uni-call.fcc-online.pl',
+  login: '<login>',
+  password: '<haslo>'
+};
+
+dialer.configure(config);  
+
 // Serwer nasłuchuje na porcie 3000
 httpServer.listen(3000, function () {
   console.log('Example app listening on port 3000!')
@@ -9,8 +17,10 @@ httpServer.listen(3000, function () {
   // gp url 3000
 })
 
-// Definiowanie odpowiedzi na request get 
-httpServer.get('/hello', function (req, res) {
-  console.log('url get /')
-  res.json({'message': 'Hello World'})
+httpServer.get('/call/:number1/:number2', (req, res) => {
+  const number1 = req.params.number1;
+  const number2 = req.params.number2;
+ 
+  dialer.call(number1,number2).catch(err => { console.log(err) } );
+  res.json({success: true});
 })
